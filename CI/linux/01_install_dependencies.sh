c8b7fbce1a6ec45df825aba626cb256657692b2c
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2022-09-03 11:32:16 -0300

    Drop V4L2 support
    Webcam support will eventually be provided by PipeWire.

--- CI/linux/01_install_dependencies.sh
+++ CI/linux/01_install_dependencies.sh
@@ -95,7 +95,7 @@ install_dependencies() {
         "qt6-deps qt6-base-dev qt6-base-private-dev libqt6svg6-dev qt6-wayland"
         "cef ${LINUX_CEF_BUILD_VERSION:-${CI_LINUX_CEF_VERSION}}"
         "plugin-deps libasound2-dev libfdk-aac-dev libfontconfig-dev libfreetype6-dev \
-         libpulse-dev libsndio-dev libspeexdsp-dev libudev-dev libv4l-dev libva-dev libvlc-dev libdrm-dev"
+         libpulse-dev libsndio-dev libspeexdsp-dev libudev-dev libva-dev libvlc-dev libdrm-dev"
     )
 
     sudo dpkg --add-architecture amd64
